8ced6e218031200ba80893dd6134727b6a5ffde6
Nick Ng <nick-ng@users.noreply.github.com>
2024-01-23 19:49:41 +1300

    fix: remove unused import

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { unixMsToJalendarMs, unixMsToJate, formatJate, formatJime } from '$lib';
+	import { unixMsToJate, formatJate, formatJime } from '$lib';
 
 	let timestamp = Date.now();
 	$: jateJime = unixMsToJate(timestamp);
